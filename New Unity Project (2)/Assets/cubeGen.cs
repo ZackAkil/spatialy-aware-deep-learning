0cccd00dec3e94e7a5b7ddf612f2f872e80bb6d9
ZackAkil <zackakil94@gmail.com>
2018-08-31 21:05:04 +0100

    init

---
+++ New Unity Project (2)/Assets/cubeGen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeGen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+		float temp = transform.position.x + 0.1f;
+        transform.position = new Vector3(temp,  0.0f, 0.0f);
+    }
+}
